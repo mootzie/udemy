3ab8175230f63dc435754afcde2d77de7ccbe035
mootzie <tylerw77156@gmail.com>
2022-12-15 11:05:59 -0600

    "first commit"

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+    <meta charset="<?php bloginfo('charset') ?>">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <?php wp_head(); ?>
+    <title>Document</title>
+    
+</head>
+<body <?php body_class('example') ?>>
+
+<?php wp_body_open(); ?>
+    <p>hello</p>
+
+    
+
+    <?php wp_footer(); ?>
+</body>
+</html>
